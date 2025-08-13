b3252049ae4309f0b437ec904d3364c04fcd7423
georgelush <100272529+georgelush@users.noreply.github.com>
2025-08-13 14:17:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,30 +2,30 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware pentru a permite parsarea JSON
+// Middleware for parsing JSON
 app.use(express.json());
 
-let stocareNumar = null; // Variabilă pentru a stoca numărul
+let dasNumber = null; // Var for das
 
-// Endpoint pentru a stoca un număr
+// Endpoint for save Das
 app.post('/postDas', (req, res) => {
-    const { numar } = req.body; // Preia numărul din cererea POST
-    if (typeof numar === 'number') {
-        stocareNumar = numar; // Stochează numărul
-        return res.status(200).json({ message: 'Numărul a fost stocat cu succes!' });
+    const { Das } = req.body; // get from body
+    if (typeof Das === 'number') {
+        dasNumber = Das; // save number
+        return res.status(200).json({ message: 'Succes dass add Dass' });
     }
-    return res.status(400).json({ message: 'Te rog să trimiți un număr valid!' });
+    return res.status(400).json({ message: 'Invalid number' });
 });
 
-// Endpoint pentru a obține numărul
+// Endpoint for get Das
 app.get('/getDas', (req, res) => {
-    if (stocareNumar !== null) {
-        return res.status(200).json({ numar: stocareNumar }); // Returnează numărul stocat
+    if (dasNumber !== null) {
+        return res.status(200).json({ Das: dasNumber }); // Returning Das
     }
-    return res.status(404).json({ message: 'Numărul nu a fost găsit!' });
+    return res.status(404).json({ message: 'Das not exist' });
 });
 
-// Pornirea serverului
+// Server start
 app.listen(PORT, () => {
-    console.log(`Serverul rulează pe portul ${PORT}`);
+    console.log(`Server is runing on ${PORT}`);
 });
